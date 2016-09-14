39208a1d84bb925875837fc29f821e09d44c0abe
Puspendu Banerjee <Puspendu.Banerjee@gmail.com>
2016-09-14 01:49:01 -0500

    Updated sample to show PropertyPlaceHolder replacement Capability

--- src/main/scala/app/actors/MainApp.scala
+++ src/main/scala/app/actors/MainApp.scala
@@ -1,15 +1,20 @@
 package app.actors
 
+import java.util.Properties
+
 import akka.actor.Props
 import akka.camel.CamelExtension
+import com.typesafe.config.ConfigFactory
 import system.SpringContextActorSystemProvider
 
 /**
   * Created by puspendu on 8/1/16.
   */
-object SingleNodeApp extends App {
+object MainApp extends App {
   // get hold of the actor system
-  implicit val system = SpringContextActorSystemProvider.create()
+  val properties=new Properties()
+  properties.setProperty("rest.port","18877")
+  implicit val system = SpringContextActorSystemProvider.create("SpringActorSystem",ConfigFactory.load(),properties)
 val camel = CamelExtension(system)
   val camelCXFRest = system.actorOf(Props[CamelCXFRestInterface], name = "CamelCXFRestInterface")
   camel.context.addRoutes(new CustomRouteBuilder(system, camelCXFRest))
